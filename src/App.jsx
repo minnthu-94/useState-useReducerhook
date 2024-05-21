666025154ca7871aca9059795944a0783a4529ec
Minnthu <minnthu.026@gmail.com>
2024-05-21 21:14:42 +0630

    use reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 
-function App() {
+const firstReducer = (state,action) => {
+  switch (action.type) {
+    case "minus":
+      return {...state,count: state.count - 1}
+    case "plus":
+      return {...state, count: state.count + 1}
+    case "updateKey":
+      return {...state, key: action.payload}
+      default:
+      throw new Error();
+  }
+}
 
-  // eslint-disable-next-line no-unused-vars
-  const [key,setKey] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [count,setCount] = useState(0)
-;
+// eslint-disable-next-line no-unused-vars
+const ACTION = {
+  PLUS : 'plus',
+  MINUS : 'minus',
+  UPDATE_KEY : "updateKey"
+}
+
+function App() {
+  const [state,dispatch] = useReducer(firstReducer,{key:"",count:0})
   return (
     <>
       <input
         type="text"
-        onChange={(e) => {
-          setKey(e.target.value);
-        }}
+        onChange={(e)=>
+          dispatch({type:ACTION.UPDATE_KEY,payload: e.target.value})
+        }
       />
-      <h1>Your key is - {key}</h1>
+      <h1>Your key is - {state.key}</h1>
 
-      <button
-        onClick={() => {
-          setCount((prev) => prev - 1);
-        }}
-      >
-        -
-      </button>
-      <span>{count}</span>
+      <button onClick={() => dispatch({ type: ACTION.MINUS })}>-</button>
+      <span>{state.count}</span>
 
-      <button
-        onClick={() => {
-          setCount((prev) => prev + 1);
-        }}
-      >
-        +
-      </button>
+      <button onClick={() => dispatch({ type: ACTION.PLUS })}>+</button>
     </>
   );
 }
